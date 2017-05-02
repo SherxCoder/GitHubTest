724f1d523edf5084645989ffcd2393808645b05e
SherxCoder <1660261925@qq.com>
2017-05-02 19:14:14 +0800

    添加了textview的点击事件

--- app/src/main/java/com/edu/swun/githubtest/MainActivity.java
+++ app/src/main/java/com/edu/swun/githubtest/MainActivity.java
@@ -2,12 +2,21 @@ package com.edu.swun.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public TextView  mText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mText= (TextView) findViewById(R.id.nihao);
+        mText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mText.setText("不好");
+            }
+        });
     }
 }
